cffab4d6f7c7d5f2507156e1160524bb9aa51727
Leon To <khoito100@gmail.com>
2020-06-10 13:22:42 -0700

    add bootstrap to nav

--- src/app/router/Router.js
+++ src/app/router/Router.js
@@ -5,20 +5,27 @@ import {
     Route,
     Link
 } from "react-router-dom";
-
+import {Nav, Navbar} from 'react-bootstrap';
 
 import './Router.css';
 import Home from '../../home/Home';
 
+
 export default class Router extends React.Component{
     render(){
         const nav = (
-            <div className="nav">
-                <img className="nav-logo" src="img/pizza-mila-logo.jpg"/>
-                <Link to="/" className="nav-home">Home</Link>
-                <div className="nav-news">News</div>
-                <div className="nav-login">Login</div>
-            </div>
+            // <div className="nav">
+            //     <img className="nav-logo" src="img/pizza-mila-logo.jpg"/>
+            //     <Link to="/" className="nav-home">Home</Link>
+            //     <div className="nav-news">News</div>
+            //     <div className="nav-login">Login</div>
+            // </div>
+            <Navbar bg="light" expand="light">
+              <Navbar.Brand href="#home">PIZZA MILA</Navbar.Brand>
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+              </Nav>
+            </Navbar>
         );
             
         const swit = (
